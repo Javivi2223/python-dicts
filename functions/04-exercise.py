287457420c170806a005eb5c430f3451c1828dce
Javiera Guerrero <javiera.guerrero@@soymas.cl>
2023-07-10 23:02:52 -0400

    funcion

--- functions/04-exercise.py
+++ functions/04-exercise.py
@@ -1,3 +1,9 @@
-from match import sqrt
+from math import sqrt
 #crear una función que retone la hipotenusa en un rectangulo
 
+def hyp(side1,side2):
+  return sqrt(side1**2 + side2**2)     
+
+result = hyp(3,4) 
+
+print(f"el resultado de la hipotenusa es {result}")
--- functions/05-exercise2.py
+++ functions/05-exercise2.py
@@ -1,4 +1,14 @@
 #crear triangulo
 
-def is_triangle(side1, sice2, side3)
-  pass 
+side_a = int(input("dime el largo del primer lado del triangulo: "))
+side_b = int(input("dime el segundo largo del triangulo: "))
+side_c = int(input("dime el tercer largo del triangulo: "))
+def is_triangle(side1, side2, side3):
+  if side1 + side2 > side3 and side2 + side3 > side1 and side3 + side1 > side2:
+    print(True)
+  else:
+    print(False)
+
+is_triangle(side_a, side_b, side_c)    
+
+ 
